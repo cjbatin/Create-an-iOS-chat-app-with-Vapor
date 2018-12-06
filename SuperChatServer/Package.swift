083fe3e3a1e23842791a65a579f422da9b4ae37a
Christopher Batin <chris@batin.me>
2018-12-06 19:05:35 +0000

    Removed push notifications depenency

--- SuperChatServer/Package.swift
+++ SuperChatServer/Package.swift
@@ -6,13 +6,12 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "git@github.com:pusher/push-notifications-server-swift.git", .branch("master")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.0.0"),
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "PushNotifications", "PerfectCrypto"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "PerfectCrypto"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
